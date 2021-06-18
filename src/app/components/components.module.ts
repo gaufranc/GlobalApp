89609a386cd532c1676e4918e26a54ec9ddd4183
Gaspar Aufranc <gaspar.aufranc@gmail.com>
2021-06-18 17:31:59 -0300

    minor improvements to the graph

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartsModule } from 'ng2-charts';
+import { LinearGraphComponent } from './linear-graph/linear-graph.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LinearGraphComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ChartsModule
+  ],
+  exports:[
+    LinearGraphComponent
   ]
 })
 export class ComponentsModule { }
--- src/app/components/linear-graph/linear-graph.component.ts
+++ src/app/components/linear-graph/linear-graph.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 
 @Component({
   selector: 'app-linear-graph',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./linear-graph.component.css']
 })
 export class LinearGraphComponent implements OnInit {
+  @Input() title: string = '';
+  @Input('labels') linearChartLabels: Label[] = ['seccion1', 'seccion2', 'seccion3']
+  @Input('data') linearChartData: MultiDataSet = [[100, 120, 140]]
 
+
+  public colors:Color[] = [
+    { backgroundColor: ['#6857E6', '#009FEE', '#F02059']}
+  ]
   constructor() { }
 
   ngOnInit(): void {
