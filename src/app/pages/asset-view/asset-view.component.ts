e3cab83f7609a8bc78c52924be58fecb1b4b4215
Gaspar Aufranc <gaspar.aufranc@gmail.com>
2021-06-21 12:55:30 -0300

    asset in-depth working

--- src/app/pages/asset-view/asset-view.component.ts
+++ src/app/pages/asset-view/asset-view.component.ts
@@ -14,9 +14,7 @@ import { Color, Label } from 'ng2-charts';
 })
 export class AssetViewComponent implements OnInit {
 
-  public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-  ];
+  public lineChartData: ChartDataSets[] = [];
   public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: (ChartOptions) = {
     responsive: true,
@@ -78,6 +76,7 @@ export class AssetViewComponent implements OnInit {
 
   finalData: any[] = [];
   finalLabel: any[] = [];
+
   constructor(public dataService: DataServiceService) { 
     this.asset = this.dataService.activeAsset;
     this.dataService.activeInstruments.forEach(instrument => {
@@ -87,22 +86,18 @@ export class AssetViewComponent implements OnInit {
           
           
           this.finalData.push({
-            data:[resp.data[0].open,resp.data[1].open,resp.data[2].open,resp.data[3].open,resp.data[4].open,resp.data[5].open,resp.data[6].open,resp.data[7].open,resp.data[8].open,resp.data[9].open, resp.data[10].open], label: resp.data[0].instrument
+            data:[resp.data[0].open,resp.data[1].open,resp.data[2].open,resp.data[3].open,resp.data[4].open,resp.data[5].open,resp.data[6].open,resp.data[7].open,resp.data[8].open,resp.data[9].open, resp.data[10].open],
+            label: resp.data[0].instrument,
           })
-          this.finalLabel.push(resp.data[0].start)
-          // [{data:[number], label:''}]
-          for (let index = 0; index < 11; index++) {
-              this.finalLabel.push(resp.data[index].start)
 
-          }
+          this.finalLabel.push(resp.data[0].start,resp.data[1].start,resp.data[2].start,resp.data[3].start,resp.data[4].start,resp.data[5].start,resp.data[6].start,resp.data[7].start,resp.data[8].start,resp.data[9].start, resp.data[10].start)
         }
         
-        //console.log(this.activeInstrumentsWithData);
-        //console.log(this.finalData);
         this.lineChartData = this.finalData;
-        
+        this.lineChartLabels = [this.finalLabel[0].slice(0, 10), this.finalLabel[1].slice(0, 10),this.finalLabel[2].slice(0, 10), this.finalLabel[3].slice(0, 10),this.finalLabel[4].slice(0, 10), this.finalLabel[5].slice(0, 10),this.finalLabel[6].slice(0, 10), this.finalLabel[7].slice(0, 10),this.finalLabel[8].slice(0, 10), this.finalLabel[9].slice(0, 10), this.finalLabel[10].slice(0, 10)]
         this.loading = false;
       })
+      
       
     });
    }
@@ -111,26 +106,3 @@ export class AssetViewComponent implements OnInit {
   }
 
 }
-
-
-// constructor(public dataService: DataServiceService) { 
-//   this.asset = this.dataService.activeAsset;
-//   this.dataService.activeInstruments.forEach(instrument => {
-//     this.dataService.getInstrumentData(instrument).subscribe((resp:any) => {
-//       if(resp.data.length > 0){
-//         this.activeInstrumentsWithData.push(resp)
-//         this.howMany++
-//         //console.log(resp.data);
-//         this.finalData.push({
-//           data:[resp.data[0].open,resp.data[1].open,resp.data[2].open,resp.data[3].open,resp.data[4].open,resp.data[5].open,resp.data[6].open,resp.data[7].open,resp.data[8].open,resp.data[9].open, resp.data[10].open], label: resp.data[0].instrument
-//         })
-//         //console.log(asd);
-//         // [{data:[number], label:''}]
-//       }
-//       //console.log(this.activeInstrumentsWithData);
-//       console.log(this.finalData);
-//       this.lineChartData = this.finalData;
-//       this.loading = false;
-//     })
-//   });
-//  }
